520bc8a9a33cf1ad91e8ce286619c2a0c8dd0882
Andrey T <fxuniverse@gmail.com>
2014-04-01 22:21:16 -0400

    add files

---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,346 @@
+'use strict';
+var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+var mountFolder = function(connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('assemble');
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // configurable paths
+    var yeomanConfig = {
+        app: 'app',
+        dist: 'dist'
+    };
+
+    grunt.initConfig({
+        yeoman: yeomanConfig,
+        assemble: {
+            options: {
+                flatten: true,
+                layout: 'default.hbs',
+                layoutdir: '<%%= yeoman.app %>/templates/layouts',
+                assets: 'dist/images',
+                partials: ['<%%= yeoman.app %>/templates/partials/*.hbs']
+            },
+            dist: {
+                files: {
+                    '<%%= yeoman.dist %>/': ['<%%= yeoman.app %>/templates/pages/*.hbs']
+                }
+            },
+            server: {
+                files: {
+                    '.tmp/': ['<%%= yeoman.app %>/templates/pages/*.hbs']
+                }
+            }
+        },
+        watch: {
+            coffee: {
+                files: ['<%%= yeoman.app %>/scripts/{,*/}*.coffee'],
+                tasks: ['coffee:dist']
+            },
+            coffeeTest: {
+                files: ['test/spec/{,*/}*.coffee'],
+                tasks: ['coffee:test']
+            },
+            compass: {
+                files: ['<%%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+                tasks: ['compass']
+            },
+            livereload: {
+                files: [
+                    '.tmp/*.html',
+                    '<%%= yeoman.app %>/*.html',
+                    '{.tmp,<%%= yeoman.app %>}/styles/{,*/}*.css',
+                    '{.tmp,<%%= yeoman.app %>}/scripts/{,*/}*.js',
+                    '<%%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}'
+                ],
+                tasks: ['livereload']
+            },
+            assemble: {
+                files: ['<%%= yeoman.app %>/templates/layouts/*.hbs',
+                    '<%%= yeoman.app %>/templates/pages/*.hbs',
+                    '<%%= yeoman.app %>/templates/partials/*.hbs'
+                ],
+                tasks: ['assemble:server']
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function(connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, 'app')
+                        ];
+                    }
+                }
+            },
+            test: {
+                options: {
+                    middleware: function(connect) {
+                        return [
+                            mountFolder(connect, 'test'),
+                            mountFolder(connect, '.tmp')
+                        ];
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    middleware: function(connect) {
+                        return [
+                            mountFolder(connect, 'dist')
+                        ];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%%= connect.options.port %>'
+            }
+        },
+        clean: {
+            dist: ['.tmp', '<%%= yeoman.dist %>/*'],
+            server: '.tmp'
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                '<%%= yeoman.app %>/scripts/{,*/}*.js',
+                '!<%%= yeoman.app %>/scripts/vendor/*',
+                '!<%%= yeoman.app %>/scripts/libs/*',
+                '!<%%= yeoman.app %>/scripts/plugins/*',
+                'test/spec/{,*/}*.js'
+            ]
+        },
+        mocha: {
+            all: {
+                options: {
+                    run: true,
+                    urls: ['http://localhost:<%%= connect.options.port %>/index.html']
+                }
+            }
+        },
+        coffee: {
+            dist: {
+                files: [{
+                    // rather than compiling multiple files here you should
+                    // require them into your main .coffee file
+                    expand: true,
+                    cwd: '<%%= yeoman.app %>/scripts',
+                    src: '*.coffee',
+                    dest: '.tmp/scripts',
+                    ext: '.js'
+                }]
+            },
+            test: {
+                files: [{
+                    expand: true,
+                    cwd: '.tmp/spec',
+                    src: '*.coffee',
+                    dest: 'test/spec'
+                }]
+            }
+        },
+        compass: {
+            options: {
+                cssDir: '.tmp/styles',
+                sassDir: '<%%= yeoman.app %>/styles',
+                imagesDir: '<%%= yeoman.app %>/images',
+                javascriptsDir: '.tmp/scripts',
+                loadAll: '<%%= yeoman.app %>/styles/extensions',
+                relativeAssets: true,
+                force: true
+            },
+            dist: {},
+            server: {
+                options: {
+                    debugInfo: true
+                }
+            }
+        },
+        // not used since Uglify task does concat,
+        // but still available if needed
+        /*concat: {
+            dist: {}
+        },*/
+        requirejs: {
+            dist: {
+                // Options: https://github.com/jrburke/r.js/blob/master/build/example.build.js
+                options: {
+                    // `name` and `out` is set by grunt-usemin
+                    baseUrl: 'app/scripts',
+                    optimize: 'none',
+                    // TODO: Figure out how to make sourcemaps work with grunt-usemin
+                    // https://github.com/yeoman/grunt-usemin/issues/30
+                    //generateSourceMaps: true,
+                    // required to support SourceMaps
+                    // http://requirejs.org/docs/errors.html#sourcemapcomments
+                    preserveLicenseComments: false,
+                    useStrict: true,
+                    wrap: true,
+                    //uglify2: {} // https://github.com/mishoo/UglifyJS2
+                }
+            }
+        },
+        useminPrepare: {
+            html: '.tmp/index.html',
+            options: {
+                dest: '<%%= yeoman.dist %>'
+            }
+        },
+        usemin: {
+            html: ['<%%= yeoman.dist %>/{,*/}*.html'],
+            css: ['<%%= yeoman.dist %>/styles/{,*/}*.css'],
+            options: {
+                dirs: ['<%%= yeoman.dist %>']
+            }
+        },
+        imagemin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%%= yeoman.app %>/images',
+                    src: '{,*/}*.{png,jpg,jpeg}',
+                    dest: '<%%= yeoman.dist %>/images'
+                }]
+            }
+        },
+        cssmin: {
+            dist: {
+                files: {
+                    '<%%= yeoman.dist %>/styles/app.css': [
+                        '.tmp/styles/{,*/}app.css',
+                        '<%%= yeoman.app %>/styles/{,*/}app.css'
+                    ],
+                    '<%%= yeoman.dist %>/styles/typography.css': [
+                        '.tmp/styles/{,*/}typography.css',
+                        '<%%= yeoman.app %>/styles/{,*/}typography.css'
+                    ]
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    /*removeCommentsFromCDATA: true,
+                    // https://github.com/yeoman/grunt-usemin/issues/44
+                    //collapseWhitespace: true,
+                    collapseBooleanAttributes: true,
+                    removeAttributeQuotes: true,
+                    removeRedundantAttributes: true,
+                    useShortDoctype: true,
+                    removeEmptyAttributes: true,
+                    removeOptionalTags: true*/
+                },
+                files: [{
+                    expand: true,
+                    cwd: '.tmp',
+                    src: '*.html',
+                    dest: '<%%= yeoman.dist %>'
+                }]
+            }
+        },
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%%= yeoman.app %>',
+                    dest: '<%%= yeoman.dist %>',
+                    src: [
+                        'fonts/{,*/}*',
+                        'scripts/libs/modernizr-2.6.2.min.js',
+                        'scripts/libs/jquery.mobile.custom.min.js',
+                        '*.{ico,txt}',
+                        '.htaccess',
+                        'images/{,*/}*.{webp,gif}'
+                    ]
+                }]
+            }
+        },
+        uglify: {
+            requirejs: {
+                files: {
+                    '<%%= yeoman.dist %>/scripts/libs/require.js': [
+                        '<%%= yeoman.app %>/scripts/libs/require.js'
+                    ]
+                }
+            }
+        },
+        bower: {
+            all: {
+                rjsConfig: '<%%= yeoman.app %>/scripts/main.js'
+            }
+        }
+    });
+
+    grunt.renameTask('regarde', 'watch');
+
+    grunt.registerTask('server', function(target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'clean:server',
+            'coffee:dist',
+            'assemble:server',
+            'compass:server',
+            'livereload-start',
+            'connect:livereload',
+            'open',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('test', [
+        'clean:server',
+        'coffee',
+        'assemble:server',
+        'compass',
+        'connect:test',
+        'mocha'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'coffee',
+        'compass:dist',
+        'assemble',
+        'useminPrepare',
+        'requirejs',
+        'imagemin',
+        'htmlmin',
+        'concat',
+        'cssmin',
+        'uglify',
+        'copy',
+        'usemin'
+    ]);
+
+    grunt.registerTask('default', [
+        'test',
+        'build'
+    ]);
+};
---
+++ app/templates/app/scripts/main.js
@@ -0,0 +1,66 @@
+require.config({
+    shim: {
+        app: {
+            deps: ['jquery', 'gumby', 'init']
+        },
+        gumby: {
+            deps: ['jquery']
+        },
+        plugins: {
+            deps: ['jquery']
+        },
+        init: {
+            deps: ['jquery', 'gumby', 'retina', 'fixed', 'skiplink', 'toggleswitch', 'checkbox', 'radiobtn', 'tabs', 'navbar', 'validation']
+        },
+        retina: {
+            deps: ['jquery', 'gumby']
+        },
+        fixed: {
+            deps: ['jquery', 'gumby']
+        },
+        skiplink: {
+            deps: ['jquery', 'gumby']
+        },
+        toggleswitch: {
+            deps: ['jquery', 'gumby']
+        },
+        checkbox: {
+            deps: ['jquery', 'gumby']
+        },
+        radiobtn: {
+            deps: ['jquery', 'gumby']
+        },
+        tabs: {
+            deps: ['jquery', 'gumby']
+        },
+        navbar: {
+            deps: ['jquery', 'gumby']
+        },
+        validation: {
+            deps: ['jquery', 'gumby']
+        }
+    },
+    paths: {
+        gumby: 'libs/gumby',
+        retina: 'libs/ui/gumby.retina',
+        fixed: 'libs/ui/gumby.fixed',
+        skiplink: 'libs/ui/gumby.skiplink',
+        toggleswitch: 'libs/ui/gumby.toggleswitch',
+        checkbox: 'libs/ui/gumby.checkbox',
+        radiobtn: 'libs/ui/gumby.radiobtn',
+        tabs: 'libs/ui/gumby.tabs',
+        navbar: 'libs/ui/gumby.navbar',
+        validation: 'libs/ui/jquery.validation',
+        // parallax: '../components/gumby-parallax/gumby.parallax',
+     /*install extension with bower, include here, as init dependancy, and shim with
+     jquery and gumby deps*/
+        init: 'libs/gumby.init',
+        jquery: 'libs/jquery-1.10.1.min',
+        plugins: 'plugins',
+        app: 'app'
+    }
+});
+
+require(['jquery', 'plugins', 'init', 'app'], function () {
+    // all app code in app.js
+});
